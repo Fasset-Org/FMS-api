cc9a773aa0f0cccee10b2787353f7e96145a7d8f
Kamzen <kamzen1994@gmail.com>
2023-05-31 14:56:07 +0200

    Create 20230531111803-modules.js

---
+++ seeders/20230531111803-modules.js
@@ -0,0 +1,40 @@
+"use strict";
+const { v4: uuidv4 } = require("uuid");
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    /**
+     * Add seed commands here.
+     *
+     * Example:
+     * await queryInterface.bulkInsert('People', [{
+     *   name: 'John Doe',
+     *   isBetaMember: false
+     * }], {});
+     */
+
+    await queryInterface.bulkInsert(
+      { schema: "wms", tableName: "modules", modelName: "Module" },
+      [
+        {
+          id: uuidv4(),
+          moduleName: "ICT Admin",
+          moduleDesc: "Same as the super admin with rights",
+          dateCreated: new Date(),
+          dateUpdated: new Date()
+        }
+      ],
+      {}
+    );
+  },
+
+  async down(queryInterface, Sequelize) {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+  }
+};
