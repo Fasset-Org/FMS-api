48b3eb202c2cfd0dc01d9bae2c65868200415da0
Kamzen <kamzen1994@gmail.com>
2023-05-31 14:56:04 +0200

    Create 20230531111743-roles.js

---
+++ seeders/20230531111743-roles.js
@@ -0,0 +1,40 @@
+"use strict";
+const { v4: uuidv4 } = require("uuid");
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    /**
+     * Add seed commands here.
+     *
+     * Example:
+     * await queryInterface.bulkInsert('People', [{
+     *   name: 'John Doe',
+     *   isBetaMember: false
+     * }], {});
+     */
+
+    await queryInterface.bulkInsert(
+      { schema: "wms", tableName: "roles", modelName: "Role" },
+      [
+        {
+          id: uuidv4(),
+          roleName: "ICT Administrator(Desktop Support Specialist)",
+          roleDesc: "Super User",
+          dateCreated: new Date(),
+          dateUpdated: new Date()
+        }
+      ],
+      {}
+    );
+  },
+
+  async down(queryInterface, Sequelize) {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+  }
+};
