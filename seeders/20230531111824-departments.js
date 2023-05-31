e0ca9c3805a94933523a899cbc9a92ac39b56ddf
Kamzen <kamzen1994@gmail.com>
2023-05-31 14:56:10 +0200

    Create 20230531111824-departments.js

---
+++ seeders/20230531111824-departments.js
@@ -0,0 +1,40 @@
+"use strict";
+const { v4: uuidv4 } = require("uuid");
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    /**
+     * Add seed commands here.
+     *
+     * Example:
+     * await queryInterface.bulkInsert('People', [{
+     *   name: 'John Doe',
+     *   isBetaMember: false
+     * }], {});
+     */
+
+    await queryInterface.bulkInsert(
+      { schema: "wms", tableName: "departments", modelName: "Department" },
+      [
+        {
+          id: uuidv4(),
+          departmentName: "Information Communication Technology(ICT)",
+          departmentDesc: "IT stuff reported here",
+          dateCreated: new Date(),
+          dateUpdated: new Date()
+        }
+      ],
+      {}
+    );
+  },
+
+  async down(queryInterface, Sequelize) {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+  }
+};
