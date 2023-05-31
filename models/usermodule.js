8afaeb472b09442613de10d1c29c0c345958d00a
Kamzen <kamzen1994@gmail.com>
2023-05-31 11:16:40 +0200

    Create usermodule.js

---
+++ models/usermodule.js
@@ -0,0 +1,53 @@
+"use strict";
+const { Model } = require("sequelize");
+module.exports = (sequelize, DataTypes) => {
+  class UserModule extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate({ User, Module }) {
+      // define association here
+      this.belongsTo(User, {
+        foreignKey: "userId"
+      });
+      this.belongsTo(Module, {
+        foreignKey: "moduleId"
+      });
+    }
+  }
+  UserModule.init(
+    {
+      id: {
+        primaryKey: true,
+        type: DataTypes.UUID,
+        defaultValue: DataTypes.UUIDV4
+      },
+      moduleId: {
+        type: DataTypes.UUID
+      },
+      userId: {
+        type: DataTypes.UUID
+      },
+      dateCreated: {
+        allowNull: false,
+        type: DataTypes.DATE
+      },
+      dateUpdated: {
+        allowNull: false,
+        type: DataTypes.DATE
+      }
+    },
+    {
+      sequelize,
+      modelName: "UserModule",
+      tableName: "userModules",
+      updatedAt: "dateUpdated",
+      createdAt: "dateCreated",
+      schema: "wms",
+      timestamps: true
+    }
+  );
+  return UserModule;
+};
