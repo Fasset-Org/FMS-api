bbee010d65c652d3ae96fa6343b433da38fd62d6
Kamzen <kamzen1994@gmail.com>
2023-05-31 11:15:53 +0200

    Create app.js

---
+++ app.js
@@ -0,0 +1,43 @@
+/** Author : Themba Makamu
+ * Date: 31-05-2023
+ **/
+const express = require("express");
+const dotenv = require("dotenv");
+const morgan = require("morgan");
+const cors = require("cors");
+const { sequelize } = require("./models");
+const appRouters = require("./routes/auth");
+
+// initialize app
+const app = express();
+
+dotenv.config({ path: `${__dirname}/config/config.env` });
+
+const port = process.env.PORT || 5000;
+
+// middlewares
+app.use(cors({ origin: true, credentials: true }));
+app.use(morgan("method :url :status :res[content-length] - :response-time ms"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+const NODE_ENV = process.env.NODE_ENV || "dev";
+
+const BASE_URL = `/api/${NODE_ENV}`;
+
+app.get("/", (req, res, next) => {
+  return res.status(200).json({
+    success: true,
+    message: "Yes, it is working"
+  });
+});
+
+// use appRouters
+
+app.use(`${BASE_URL}`, appRouters);
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+  sequelize.authenticate();
+  console.log(`Database connected on port ${5432}`);
+});
