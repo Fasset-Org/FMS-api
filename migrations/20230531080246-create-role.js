a5fcf8a83ac806dbda890f2f3decd9920a45b267
Kamzen <kamzen1994@gmail.com>
2023-05-31 11:16:19 +0200

    Create 20230531080246-create-role.js

---
+++ migrations/20230531080246-create-role.js
@@ -0,0 +1,31 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Roles', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      fullName: {
+        type: Sequelize.TEXT
+      },
+      userName: {
+        type: Sequelize.TEXT
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, DataTypes) {
+    await queryInterface.dropTable('Roles');
+  }
+};
