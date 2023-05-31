5982512e96fa1a8c7b1c6351c56e701101b0989d
Kamzen <kamzen1994@gmail.com>
2023-05-31 14:55:37 +0200

    Update 20230531080246-create-role.js

--- migrations/20230531080246-create-role.js
+++ migrations/20230531080246-create-role.js
@@ -1,31 +1,36 @@
-'use strict';
+"use strict";
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
-  async up(queryInterface, Sequelize) {
-    await queryInterface.createTable('Roles', {
-      id: {
-        allowNull: false,
-        autoIncrement: true,
-        primaryKey: true,
-        type: Sequelize.INTEGER
-      },
-      fullName: {
-        type: Sequelize.TEXT
-      },
-      userName: {
-        type: Sequelize.TEXT
-      },
-      createdAt: {
-        allowNull: false,
-        type: Sequelize.DATE
-      },
-      updatedAt: {
-        allowNull: false,
-        type: Sequelize.DATE
+  async up(queryInterface, DataTypes) {
+    await queryInterface.createTable(
+      { schema: "wms", tableName: "roles" },
+      {
+        id: {
+          primaryKey: true,
+          type: DataTypes.UUID
+        },
+        roleName: {
+          type: DataTypes.STRING
+        },
+        roleDesc: {
+          type: DataTypes.STRING
+        },
+        dateCreated: {
+          allowNull: false,
+          type: DataTypes.DATE
+        },
+        dateUpdated: {
+          allowNull: false,
+          type: DataTypes.DATE
+        }
       }
-    });
+    ),
+      {
+        schema: "wms",
+        tableName: "roles"
+      };
   },
   async down(queryInterface, DataTypes) {
-    await queryInterface.dropTable('Roles');
+    await queryInterface.dropTable({ schema: "wms", tableName: "roles" });
   }
 };
