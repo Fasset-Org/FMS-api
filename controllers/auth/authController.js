563c9384f35d4b553731732624fd96ba39842964
Kamzen <kamzen1994@gmail.com>
2023-06-01 12:23:57 +0200

    Update authController.js

--- controllers/auth/authController.js
+++ controllers/auth/authController.js
@@ -1,12 +1,18 @@
-const { sequelize, User, UserModule } = require("../../models");
+const {
+  sequelize,
+  User,
+  UserModule,
+  Role,
+  Module,
+  Department
+} = require("../../models");
 const { ApiResponse, ApiError } = require("../../utils/response");
 const sendEmail = require("../../utils/sendEmail");
-const crypto = require("crypto");
 const { Op } = require("sequelize");
+const bcryptjs = require("bcryptjs");
+const helper = require("../../utils/helper");
 
 exports.addUser = async (req, res, next) => {
-  const t = await sequelize.transaction();
-
   try {
     // check if user exist
     const userExist = await User.findOne({
@@ -19,31 +25,108 @@ exports.addUser = async (req, res, next) => {
     if (userExist) throw new ApiError("Email/Username already taken", 409);
 
     // create reset password token
-    const resetPasswordToken = crypto.randomBytes(48).toString("hex");
-
-    // create user
-    const user = await User.create(
-      { ...req.body, resetPasswordToken: resetPasswordToken },
-      { transaction: t }
+    // const resetPasswordToken = crypto.randomBytes(48).toString("hex");
+    const resetPasswordToken = helper.getJWTtoken(
+      { email: req.body.email },
+      process.env.JWT_RESET_KEY,
+      `${60 * 7}s`
     );
 
-    // create userModules
-    req.body.userModules.forEach(async (userModuleId) => {
-      await UserModule.create(
-        {
-          userId: user.id,
-          moduleId: userModuleId
-        },
-        { transaction: t }
-      );
+    // create user
+    const user = await User.create({
+      ...req.body,
+      resetPasswordToken: resetPasswordToken
     });
-    await t.commit();
 
-    const html = sendEmail({});
+    const html = `
+      Dear ${user.email} <br /> 
+      Please note that you have been added to FMS, if you are not aware of  
+      this action please contact devsupport@fasset.org.za <br /> or. <br /> 
+      Click <a href="${process.env.APP_URL}/resetPassword/${resetPasswordToken}">here</a> to reset your password
+    `;
+
+    sendEmail({
+      email: user.email,
+      subject: "FMS Password reset",
+      html: html
+    });
+
+    return res
+      .status(201)
+      .json(ApiResponse("User added successfully", "user", user));
   } catch (err) {
-    t.rollback();
+    console.log(err);
     next(err);
   }
 };
 
-exports.login = (req, res, next) => {};
+exports.login = async (req, res, next) => {
+  try {
+    const { email, password } = req.body;
+
+    console.log(email, password);
+
+    const user = await User.findOne({
+      where: {
+        [Op.or]: [{ email: email }, { userName: email }]
+      },
+      raw: true,
+      nest: true,
+      // attributes: {
+      //   exclude: ["password"]
+      // },
+      include: [
+        { model: Role, as: 'role' },
+        { model: Department, as: 'department' },
+        {
+          model: UserModule,
+          as: 'userModules',
+          include: [{ model: Module, as: 'module' }]
+        }
+      ]
+    });
+
+    if (!user) throw new ApiError("Invalid credentials", 404);
+
+    // check password
+    const isPasswordCorrect = await bcryptjs.compare(password, user.password);
+
+    if (!isPasswordCorrect) throw new ApiError("Invalid credentials", 404);
+
+    const token = helper.getJWTtoken(
+      { ...user, password: "" },
+      process.env.JWT_ACCESS_KEY,
+      `420s`
+    );
+
+    const refreshToken = helper.getJWTtoken(
+      { ...user, password: "" },
+      process.env.JWT_REFRESH_KEY
+    );
+
+    res.cookie(
+      process.env.COOKIE_ACCESS_TOKEN,
+      token,
+      helper.SESSION_COOKIE_OPTIONS
+    );
+    res.cookie(
+      process.env.COOKIE_REFRESH_TOKEN,
+      refreshToken,
+      helper.REFRESH_SESSION_COOKIE_OPTIONS
+    );
+
+    return res.status(200).json(
+      ApiResponse("User login in successfully", "user", {
+        ...user,
+        password: "",
+        token: token,
+        refreshToken: refreshToken
+      })
+    );
+  } catch (err) {
+    console.log(err);
+    next(err);
+  }
+};
+
+exports.resetPassword = async (req, res, next) => {};
