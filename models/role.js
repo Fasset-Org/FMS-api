530313e6a1076fefd45c3e9e63a539df3db90fe9
Kamzen <kamzen1994@gmail.com>
2023-06-01 12:23:35 +0200

    Update role.js

--- models/role.js
+++ models/role.js
@@ -9,7 +9,7 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate({ User }) {
       // define association here
-      this.belongsTo(User, {
+      this.hasMany(User, {
         foreignKey: "roleId"
       });
     }
