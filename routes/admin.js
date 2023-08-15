8e7e231dbcc2c69a3aa8f6195d111ea3b62f7873
Kamzen <kamzen1994@gmail.com>
2023-08-15 21:12:45 +0200

    Create admin.js

---
+++ routes/admin.js
@@ -0,0 +1,17 @@
+const {Router} = require('express');
+const AuthMid = require('../middlewares/authMid');
+const { addDepartment, editDepartment, getAllDepartments, addModule, editModule, getAllModules } = require('../controllers/admin/adminController');
+
+const adminRouter = Router()
+
+// Department routes
+adminRouter.post('/department', AuthMid, addDepartment);
+adminRouter.post('/department/:departmentId', AuthMid, editDepartment);
+adminRouter.get('/departments', AuthMid, getAllDepartments)
+
+// Module routes
+adminRouter.post('/module', AuthMid, addModule);
+adminRouter.post('/module/:moduleId', AuthMid, editModule);
+adminRouter.get('/modules', AuthMid, getAllModules);
+
+module.exports = adminRouter
