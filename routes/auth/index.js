2b9b441e19d293e7907e5eeaaa344d586e67d39f
Kamzen <kamzen1994@gmail.com>
2023-08-15 21:12:41 +0200

    Update index.js

--- routes/auth/index.js
+++ routes/auth/index.js
@@ -1,8 +1,10 @@
 const { Router } = require("express");
 const authRouter = require("./auth");
+const adminRouter = require("../admin");
 
 const appRouters = Router();
 
 appRouters.use("/auth", authRouter);
+appRouters.use("/admin", adminRouter)
 
 module.exports = appRouters;
