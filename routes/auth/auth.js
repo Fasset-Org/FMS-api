5ef4047086137a4af0ef02007172455ce9553f6f
Kamzen <kamzen1994@gmail.com>
2023-08-18 13:54:20 +0200

    Update auth.js

--- routes/auth/auth.js
+++ routes/auth/auth.js
@@ -2,14 +2,16 @@ const { Router } = require("express");
 const {
   loginUser,
   addUser,
-  isUserLoggedIn
+  isUserLoggedIn,
+  verifyResetToken
 } = require("../../controllers/auth/authController");
 const AuthMid = require("../../middlewares/authMid");
 const authRouter = Router();
 
 authRouter.post("/login", loginUser);
 
-authRouter.post("/addUser", addUser);
+authRouter.post("/addUser", AuthMid, addUser);
 authRouter.get("/isUserLoggedIn", AuthMid, isUserLoggedIn);
+authRouter.post('/verifyResetToken', verifyResetToken)
 
 module.exports = authRouter;
