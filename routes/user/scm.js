1c8918b3845eb9b9293fd7ea6dbfc39e31d0a1af
Kamzen <kamzen1994@gmail.com>
2023-08-23 19:48:15 +0200

    Update scm.js

--- routes/user/scm.js
+++ routes/user/scm.js
@@ -1,9 +1,17 @@
 const { Router } = require("express");
 const AuthMid = require("../../middlewares/authMid");
-const { addTender } = require("../../controllers/user/scmController");
+const {
+  addTender,
+  getAllCurrentTenders,
+  getAllPreviousTenders,
+  editTender
+} = require("../../controllers/user/scmController");
 
 const scmRouter = Router();
 
 scmRouter.post("/tender", AuthMid, addTender);
+scmRouter.get("/currentTenders", AuthMid, getAllCurrentTenders);
+scmRouter.get("/previousTenders", AuthMid, getAllPreviousTenders);
+scmRouter.put("/tender", AuthMid, editTender);
 
 module.exports = scmRouter;
