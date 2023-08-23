50409c309504c5da75f33b8b288fa9ce5d7f7459
Kamzen <kamzen1994@gmail.com>
2023-08-23 10:56:43 +0200

    Create tender.js

---
+++ models/tender.js
@@ -0,0 +1,90 @@
+"use strict";
+const { Model } = require("sequelize");
+module.exports = (sequelize, DataTypes) => {
+  class Tender extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  Tender.init(
+    {
+      id: {
+        allowNull: false,
+        primaryKey: true,
+        type: DataTypes.UUID
+      },
+      tenderName: {
+        type: DataTypes.TEXT,
+        allowNull: false
+      },
+      tenderReference: {
+        type: DataTypes.TEXT,
+        allowNull: false
+      },
+      invitationMessage: {
+        type: DataTypes.TEXT,
+        allowNull: false
+      },
+      bidMessage: {
+        type: DataTypes.TEXT,
+        allowNull: false
+      },
+      queryEmail: {
+        type: DataTypes.TEXT,
+        allowNull: false
+      },
+      closingDate: {
+        type: DataTypes.DATE,
+        allowNull: false
+      },
+      meetingLink: {
+        type: DataTypes.TEXT
+      },
+      meetinngId: {
+        type: DataTypes.TEXT
+      },
+      meetingLink: {
+        type: DataTypes.TEXT
+      },
+      meetingPasscode: {
+        type: DataTypes.TEXT,
+        allowNull: false
+      },
+      meetingDate: {
+        type: DataTypes.DATE
+      },
+      tenderDocument: {
+        type: DataTypes.TEXT,
+        allowNull: false
+      },
+      bidders: {
+        type: DataTypes.ARRAY(DataTypes.JSON)
+      },
+      tenderStatus: {
+        type: DataTypes.BOOLEAN,
+        allowNull: false
+      },
+      createdAt: {
+        allowNull: false,
+        type: DataTypes.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: DataTypes.DATE
+      }
+    },
+    {
+      sequelize,
+      modelName: "Tender",
+      tableName: "tenders",
+      schema: "wms",
+      timestamps: true
+    }
+  );
+  return Tender;
+};
