6c9ae3d4cf9cdc25f93ba5753895c1038fa576a0
Kamzen <kamzen1994@gmail.com>
2023-08-30 10:17:54 +0200

    Create qualification.js

---
+++ models/qualification.js
@@ -0,0 +1,47 @@
+"use strict";
+const { Model } = require("sequelize");
+module.exports = (sequelize, DataTypes) => {
+  class Qualification extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  Qualification.init(
+    {
+      id: {
+        allowNull: false,
+        primaryKey: true,
+        type: DataTypes.UUID,
+        defaultValue: DataTypes.UUIDV4
+      },
+      qualificationName: {
+        type: DataTypes.TEXT,
+        allowNull: false
+      },
+      qualificationLevel: {
+        type: DataTypes.TEXT,
+        allowNull: false
+      },
+
+      createdAt: {
+        allowNull: false,
+        type: DataTypes.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: DataTypes.DATE
+      }
+    },
+    {
+      sequelize,
+      modelName: "Qualification",
+      schema: "wms"
+    }
+  );
+  return Qualification;
+};
