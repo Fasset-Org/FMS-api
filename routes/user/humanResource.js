93a1eb1916fad0613d35d645ce4935e045b788be
Kamzen <kamzen1994@gmail.com>
2023-09-06 14:50:34 +0200

    Update humanResource.js

--- routes/user/humanResource.js
+++ routes/user/humanResource.js
@@ -1,11 +1,25 @@
 const { Router } = require("express");
 const AuthMid = require("../../middlewares/authMid");
 const {
-  addQualification
+  addQualification,
+  getAllQualifications,
+  addPositionQuestion,
+  addPosition,
+  getAllPositions,
+  editPosition,
+  getAllPositionById
 } = require("../../controllers/user/humanResourceController");
 
 const HumanResourceRouter = Router();
 
 HumanResourceRouter.post("/qualification", AuthMid, addQualification);
+HumanResourceRouter.get("/qualifications", AuthMid, getAllQualifications);
+HumanResourceRouter.post("/position", AuthMid, addPosition);
+HumanResourceRouter.put('/position/:positionId', AuthMid, editPosition);
+HumanResourceRouter.get('/position/:positionId', AuthMid, getAllPositionById)
+HumanResourceRouter.get('/positions', AuthMid, getAllPositions);
+HumanResourceRouter.post("/positionQuestion", AuthMid, addPositionQuestion);
+
+
 
 module.exports = HumanResourceRouter;
