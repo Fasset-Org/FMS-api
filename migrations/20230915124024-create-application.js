05fc66bae838ae9b6cccc97425a961791309a64b
Kamzen <kamzen1994@gmail.com>
2023-09-18 15:09:19 +0200

    Create 20230915124024-create-application.js

---
+++ migrations/20230915124024-create-application.js
@@ -0,0 +1,78 @@
+"use strict";
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, DataTypes) {
+    await queryInterface.createTable(
+      { schema: "wms", tableName: "applications" },
+      {
+        id: {
+          allowNull: false,
+          primaryKey: true,
+          type: DataTypes.UUID
+        },
+        fullname: {
+          type: DataTypesDataTypes.TEXT,
+          allowNull: false
+        },
+        email: {
+          type: DataTypes.TEXT,
+          allowNull: false
+        },
+        nationality: {
+          type: DataTypes.TEXT,
+          allowNull: false
+        },
+        idNumber: {
+          type: DataTypes.TEXT,
+          allowNull: false
+        },
+        gender: {
+          type: DataTypes.TEXT,
+          allowNull: false
+        },
+        cellphone: {
+          type: DataTypes.TEXT,
+          allowNull: false
+        },
+        idDocumentName: {
+          type: DataTypes.TEXT,
+          allowNull: false
+        },
+        resumeDocumentName: {
+          type: DataTypes.TEXT,
+          allowNull: false
+        },
+        matricDocumentName: {
+          type: DataTypes.TEXT,
+          allowNull: false
+        },
+        qualificationDocumentName: {
+          type: DataTypes.TEXT,
+          allowNull: false
+        },
+        status: {
+          type: DataTypes.TEXT,
+          allowNull: false
+        },
+        rejectReason: {
+          type: DataTypes.TEXT,
+          allowNull: false
+        },
+        createdAt: {
+          allowNull: false,
+          type: DataTypes.DATE
+        },
+        updatedAt: {
+          allowNull: false,
+          type: DataTypes.DATE
+        }
+      }
+    );
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable({
+      schema: "wms",
+      tableName: "applications"
+    });
+  }
+};
