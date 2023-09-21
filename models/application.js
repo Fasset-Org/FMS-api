39f914bc47bab322fec7cff38fffeda33d1091e5
Kamzen <kamzen1994@gmail.com>
2023-09-21 10:58:00 +0200

    Update application.js

--- models/application.js
+++ models/application.js
@@ -1,7 +1,5 @@
-'use strict';
-const {
-  Model
-} = require('sequelize');
+"use strict";
+const { Model } = require("sequelize");
 module.exports = (sequelize, DataTypes) => {
   class Application extends Model {
     /**
@@ -9,17 +7,87 @@ module.exports = (sequelize, DataTypes) => {
      * This method is not a part of Sequelize lifecycle.
      * The `models/index` file will call this method automatically.
      */
-    static associate(models) {
+    static associate({Position}) {
       // define association here
+      this.belongsTo(Position, {
+        foreignKey: 'positionId'
+      })
     }
   }
-  Application.init({
-    firstName: DataTypes.STRING,
-    lastName: DataTypes.STRING,
-    email: DataTypes.STRING
-  }, {
-    sequelize,
-    modelName: 'Application',
-  });
+  Application.init(
+    {
+      id: {
+        allowNull: false,
+        primaryKey: true,
+        type: DataTypes.UUID,
+        defaultValue: DataTypes.UUIDV4
+      },
+      fullname: {
+        type: DataTypes.TEXT,
+        allowNull: false
+      },
+      email: {
+        type: DataTypes.TEXT,
+        allowNull: false
+      },
+      nationality: {
+        type: DataTypes.TEXT,
+        allowNull: false
+      },
+      idNumber: {
+        type: DataTypes.TEXT,
+        allowNull: false
+      },
+      gender: {
+        type: DataTypes.TEXT,
+        allowNull: false
+      },
+      cellphone: {
+        type: DataTypes.TEXT,
+        allowNull: false
+      },
+      idDocumentName: {
+        type: DataTypes.TEXT,
+        allowNull: false
+      },
+      resumeDocumentName: {
+        type: DataTypes.TEXT,
+        allowNull: false
+      },
+      matricDocumentName: {
+        type: DataTypes.TEXT,
+        allowNull: false
+      },
+      qualificationDocumentName: {
+        type: DataTypes.TEXT,
+        allowNull: false
+      },
+      status: {
+        type: DataTypes.TEXT,
+        allowNull: false
+      },
+      rejectReason: {
+        type: DataTypes.TEXT,
+      },
+      positionId: {
+        type: DataTypes.UUID,
+        allowNull: false
+      },
+      createdAt: {
+        allowNull: false,
+        type: DataTypes.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: DataTypes.DATE
+      }
+    },
+    {
+      sequelize,
+      modelName: "Application",
+      tableName: 'applications',
+      schema: 'wms'
+    }
+  );
   return Application;
 };
