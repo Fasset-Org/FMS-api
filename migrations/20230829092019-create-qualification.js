d43c8db065562b67f5679d0d5770515939561d0a
Kamzen <kamzen1994@gmail.com>
2023-09-21 10:57:45 +0200

    Update 20230829092019-create-qualification.js

--- migrations/20230829092019-create-qualification.js
+++ migrations/20230829092019-create-qualification.js
@@ -33,7 +33,7 @@ module.exports = {
   async down(queryInterface, Sequelize) {
     await queryInterface.dropTable({
       schema: "wms",
-      tableName: "Qualifications"
+      tableName: "qualifications"
     });
   }
 };
