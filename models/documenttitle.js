0414009289e68733ff0b07483ea19c2ea398dbc0
Kamzen <kamzen1994@gmail.com>
2023-09-29 15:26:14 +0200

    Create documenttitle.js

---
+++ models/documenttitle.js
@@ -0,0 +1,43 @@
+"use strict";
+const { Model } = require("sequelize");
+module.exports = (sequelize, DataTypes) => {
+  class DocumentTitle extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  DocumentTitle.init(
+    {
+      id: {
+        allowNull: false,
+        primaryKey: true,
+        type: DataTypes.UUID,
+        defaultValue: DataTypes.UUIDV4
+      },
+      title: {
+        type: DataTypes.TEXT,
+        allowNull: false
+      },
+      createdAt: {
+        allowNull: false,
+        type: DataTypes.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: DataTypes.DATE
+      }
+    },
+    {
+      sequelize,
+      modelName: "DocumentTitle",
+      schema: "wms",
+      timestamps: true
+    }
+  );
+  return DocumentTitle;
+};
