324f02e5e6fabc79b42e5141139a0c93f151dafe
Kamzen <kamzen1994@gmail.com>
2023-10-11 13:59:57 +0200

    Update 20230929065248-create-documents.js

--- migrations/20230929065248-create-documents.js
+++ migrations/20230929065248-create-documents.js
@@ -2,35 +2,38 @@
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface, DataTypes) {
-    await queryInterface.createTable("Documents", {
-      id: {
-        allowNull: false,
-        primaryKey: true,
-        type: DataTypes.UUID
-      },
-      originalFileName: {
-        type: DataTypes.TEXT,
-        allowNull: false
-      },
-      fileName: {
-        type: DataTypes.TEXT,
-        allowNull: false
-      },
-      documentTitleId: {
-        type: DataTypes.UUID,
-        allowNull: false
-      },
-      createdAt: {
-        allowNull: false,
-        type: DataTypes.DATE
-      },
-      updatedAt: {
-        allowNull: false,
-        type: DataTypes.DATE
+    await queryInterface.createTable(
+      { schema: "wms", tableName: "documents" },
+      {
+        id: {
+          allowNull: false,
+          primaryKey: true,
+          type: DataTypes.UUID
+        },
+        originalFileName: {
+          type: DataTypes.TEXT,
+          allowNull: false
+        },
+        fileName: {
+          type: DataTypes.TEXT,
+          allowNull: false
+        },
+        documentTitleId: {
+          type: DataTypes.UUID,
+          allowNull: false
+        },
+        createdAt: {
+          allowNull: false,
+          type: DataTypes.DATE
+        },
+        updatedAt: {
+          allowNull: false,
+          type: DataTypes.DATE
+        }
       }
-    });
+    );
   },
   async down(queryInterface, DataTypes) {
-    await queryInterface.dropTable("Documents");
+    await queryInterface.dropTable({ schema: "wms", tableName: "documents" });
   }
 };
