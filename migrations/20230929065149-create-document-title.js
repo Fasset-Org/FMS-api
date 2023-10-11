18eea9b6f9978b4828159c1e02742942b5b1f0fc
Kamzen <kamzen1994@gmail.com>
2023-10-11 14:00:00 +0200

    Update 20230929065149-create-document-title.js

--- migrations/20230929065149-create-document-title.js
+++ migrations/20230929065149-create-document-title.js
@@ -1,28 +1,34 @@
-'use strict';
+"use strict";
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface, DataTypes) {
-    await queryInterface.createTable('DocumentTitles', {
-      id: {
-        allowNull: false,
-        primaryKey: true,
-        type: DataTypes.UUID
-      },
-      title: {
-        type: DataTypes.TEXT,
-        allowNull: false
-      },
-      createdAt: {
-        allowNull: false,
-        type: DataTypes.DATE
-      },
-      updatedAt: {
-        allowNull: false,
-        type: DataTypes.DATE
+    await queryInterface.createTable(
+      { schema: "wms", tableName: "documentTitles" },
+      {
+        id: {
+          allowNull: false,
+          primaryKey: true,
+          type: DataTypes.UUID
+        },
+        title: {
+          type: DataTypes.TEXT,
+          allowNull: false
+        },
+        createdAt: {
+          allowNull: false,
+          type: DataTypes.DATE
+        },
+        updatedAt: {
+          allowNull: false,
+          type: DataTypes.DATE
+        }
       }
-    });
+    );
   },
   async down(queryInterface, Sequelize) {
-    await queryInterface.dropTable('DocumentTitles');
+    await queryInterface.dropTable({
+      schema: "wms",
+      tableName: "documentTitles"
+    });
   }
 };
