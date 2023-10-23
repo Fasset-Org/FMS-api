d3ae77883f889ccebc90007ecd266f72a51a44a7
Kamzen <kamzen1994@gmail.com>
2023-10-23 11:26:38 +0200

    Update cse.js

--- routes/user/cse.js
+++ routes/user/cse.js
@@ -2,12 +2,22 @@ const { Router } = require("express");
 const AuthMid = require("../../middlewares/authMid");
 const {
   addDocumentTitle,
-  getAllDocumentTitles
+  getAllDocumentTitles,
+  editDownloadsTitle,
+  getAllDocumentTitleById,
+  addDocument,
+  getAllDocuments,
+  deleteDocument
 } = require("../../controllers/user/cseController");
 
 const CSERouter = Router();
 
 CSERouter.post("/downloadsTitle", AuthMid, addDocumentTitle);
 CSERouter.get("/downloadsTitle", AuthMid, getAllDocumentTitles);
+CSERouter.put('/downloadsTitle/:downloadsTitleId', AuthMid, editDownloadsTitle);
+CSERouter.get('/documentTitle/:titleId', AuthMid, getAllDocumentTitleById);
+CSERouter.post('/addDocument', AuthMid, addDocument);
+CSERouter.get('/documents', AuthMid, getAllDocuments);
+CSERouter.delete('/deleteDocument/:documentId', AuthMid, deleteDocument)
 
 module.exports = CSERouter;
