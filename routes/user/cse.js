360ff3f02358cc8ceb1e508b949e4e3093aa907d
Kamzen <kamzen1994@gmail.com>
2023-11-08 08:45:46 +0200

    Update cse.js

--- routes/user/cse.js
+++ routes/user/cse.js
@@ -7,11 +7,18 @@ const {
   getAllDocumentTitleById,
   addDocument,
   getAllDocuments,
-  deleteDocument
+  deleteDocument,
+  addGeneralNotice,
+  editGeneralNotice,
+  deleteGeneralNotice,
+  addGrantWindow,
+  editGrantWindow,
+  deleteGrantWindow
 } = require("../../controllers/user/cseController");
 
 const CSERouter = Router();
 
+// Downloads routes
 CSERouter.post("/downloadsTitle", AuthMid, addDocumentTitle);
 CSERouter.get("/downloadsTitle", AuthMid, getAllDocumentTitles);
 CSERouter.put('/downloadsTitle/:downloadsTitleId', AuthMid, editDownloadsTitle);
@@ -19,5 +26,14 @@ CSERouter.get('/documentTitle/:titleId', AuthMid, getAllDocumentTitleById);
 CSERouter.post('/addDocument', AuthMid, addDocument);
 CSERouter.get('/documents', AuthMid, getAllDocuments);
 CSERouter.delete('/deleteDocument/:documentId', AuthMid, deleteDocument)
+
+// Notice Board routes
+CSERouter.post('/generalNotice', AuthMid, addGeneralNotice);
+CSERouter.put('/generalNotice/:generalNoticeId', AuthMid, editGeneralNotice);
+CSERouter.delete('/generalNotice/:generalNoticeId', AuthMid, deleteGeneralNotice);
+
+CSERouter.post('/grantWindow', AuthMid, addGrantWindow);
+CSERouter.put('/grantWindow/:grantWindowId', AuthMid, editGrantWindow);
+CSERouter.delete('/grantWindow/:grantWindowId', AuthMid, deleteGrantWindow);
 
 module.exports = CSERouter;
