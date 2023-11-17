883c79cd60cf6b67d4bc07f632d4997d5d279442
Kamzen <kamzen1994@gmail.com>
2023-11-17 10:41:05 +0200

    Update generalnotice.js

--- models/generalnotice.js
+++ models/generalnotice.js
@@ -43,6 +43,7 @@ module.exports = (sequelize, DataTypes) => {
       sequelize,
       modelName: "GeneralNotice",
       tableName: "generalNotices",
+      schema: 'wms',
       timestamps: true
     }
   );
