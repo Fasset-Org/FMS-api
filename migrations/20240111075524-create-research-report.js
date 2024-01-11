175bf9d6db4341ae96e71d65d10c41af35fb882d
Kamzen <kamzen1994@gmail.com>
2024-01-11 10:30:13 +0200

    Create 20240111075524-create-research-report.js

---
+++ migrations/20240111075524-create-research-report.js
@@ -0,0 +1,42 @@
+"use strict";
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, DataTypes) {
+    await queryInterface.createTable(
+      { tableName: "researchReports", schema: "wms" },
+      {
+        id: {
+          allowNull: false,
+          primaryKey: true,
+          type: DataTypes.UUID
+        },
+        documentTitle: {
+          type: DataTypes.TEXT,
+          allowNull: false
+        },
+        documentDesc: {
+          type: DataTypes.TEXT,
+          allowNull: false
+        },
+        reserachReportFileURL: {
+          type: DataTypes.TEXT,
+          allowNull: false
+        },
+        createdAt: {
+          allowNull: false,
+          type: DataTypes.DATE
+        },
+        updatedAt: {
+          allowNull: false,
+          type: DataTypes.DATE
+        }
+      }
+    );
+  },
+  async down(queryInterface, DataTypes) {
+    await queryInterface.dropTable({
+      tableName: "researchReports",
+      schema: "wms"
+    });
+  }
+};
