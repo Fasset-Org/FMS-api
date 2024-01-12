b7e0d3a7a427b6792ccaf7019ee6441ef2aa779d
Kamzen <kamzen1994@gmail.com>
2024-01-12 13:35:48 +0200

    Update cse.js

--- routes/user/cse.js
+++ routes/user/cse.js
@@ -15,7 +15,26 @@ const {
   editGrantWindow,
   deleteGrantWindow,
   getAllGeneralNotices,
-  getAllGrantWindows
+  getAllGrantWindows,
+  addBannerImage,
+  deleteBannerImage,
+  getAllBannerImages,
+  addBoardMember,
+  editBoardMember,
+  getAllBoardMembers,
+  deleteBoardMember,
+  addCommitteeMember,
+  editCommitteeMember,
+  getAllCommitteeMembers,
+  deleteCommitteeMember,
+  addAnnualReport,
+  editAnnualReport,
+  getAllAnnualReports,
+  deleteAnnualReport,
+  addResearchReport,
+  editResearchReport,
+  getAllResearchReports,
+  deleteResearchReport
 } = require("../../controllers/user/cseController");
 
 const CSERouter = Router();
@@ -40,5 +59,34 @@ CSERouter.post('/grantWindow', AuthMid, addGrantWindow);
 CSERouter.put('/grantWindow/:grantWindowId', AuthMid, editGrantWindow);
 CSERouter.delete('/grantWindow/:grantWindowId', AuthMid, deleteGrantWindow);
 CSERouter.get('/grantWindows', AuthMid, getAllGrantWindows);
+
+// Banner Upload Routes 
+CSERouter.post('/banner', AuthMid, addBannerImage);
+CSERouter.get('/banners', AuthMid, getAllBannerImages);
+CSERouter.delete('/banner/:bannerImageId', AuthMid, deleteBannerImage);
+
+// Board Member Upload Routes 
+CSERouter.post('/boardMember', AuthMid, addBoardMember);
+CSERouter.put('/boardMember/:boardMemberId', AuthMid, editBoardMember);
+CSERouter.get('/boardMembers', AuthMid, getAllBoardMembers);
+CSERouter.delete('/boardMember/:boardMemberId', AuthMid, deleteBoardMember);
+
+// Committee Member Upload Routes
+CSERouter.post('/committeeMember', AuthMid, addCommitteeMember);
+CSERouter.put('/committeeMember/:committeeMemberId', AuthMid, editCommitteeMember);
+CSERouter.get('/committeeMembers', AuthMid, getAllCommitteeMembers);
+CSERouter.delete('/committeeMember/:committeeMemberId', AuthMid, deleteCommitteeMember);
+
+// Annual Reports Routes
+CSERouter.post('/annualReport', AuthMid, addAnnualReport);
+CSERouter.put('/annualReport/:annualReportId', AuthMid, editAnnualReport);
+CSERouter.get('/annualReports', AuthMid, getAllAnnualReports);
+CSERouter.delete('/annualReports/:annualReportId', AuthMid, deleteAnnualReport);
+
+// Research Reports Routes
+CSERouter.post('/researchReport', AuthMid, addResearchReport);
+CSERouter.put('/researchReport/:researchReportId', AuthMid, editResearchReport);
+CSERouter.get('/researchReports', AuthMid, getAllResearchReports);
+CSERouter.delete('/researchReports/:researchReportId', AuthMid, deleteResearchReport);
 
 module.exports = CSERouter;
