832ec6c03d80c510aa78ead1692eb6ef848cd2dc
SiyamthandaNyathi <131876850+SiyamthandaNyathi@users.noreply.github.com>
2024-04-23 10:59:58 +0200

    Update assetManagementController.js

--- controllers/user/assetManagementController.js
+++ controllers/user/assetManagementController.js
@@ -1,64 +1,13 @@
-import { Department, Device, User } from "../../models";
-import Device from "../../models/device";
-import { ApiError, ApiResponse } from "../../utils/response";
+const { ApiError, ApiResponse } = require("../../utils/response");
+const { Device } = require("../../models");
+const { LicenseSubscription } = require("../../models");
+const { Simcards, User, Role, Department } = require("../../models");
+const e = require("cors");
 
-const DeviceController = {
-  addDepartment: async (req, res, next) => {
-    try {
-      const department = await Department.create({ ...req.body });
-
-      if (!department) throw new ApiError("Error creating department", 400);
-
-      return res
-        .status(201)
-        .json(ApiResponse("Department created successfully"));
-    } catch (e) {
-      console.log(e);
-      next(e);
-    }
-  },
-  editDepartment: async (req, res, next) => {
-    try {
-      const { departmentId } = req.params;
-      const department = await Department.findOne({
-        where: { id: departmentId }
-      });
-
-      if (!department) throw new ApiError("Department not found", 404);
-
-      await department.update({ ...req.body });
-
-      return res
-        .status(200)
-        .json(ApiResponse("Department updated successfully"));
-    } catch (e) {
-      console.log(e);
-      next(e);
-    }
-  },
-
-  getAllDepartments: async (req, res, next) => {
-    try {
-      const departments = await Department.findAll();
-
-      return res
-        .status(200)
-        .json(
-          ApiResponse(
-            "All departments fecthed successfully",
-            "departments",
-            departments
-          )
-        );
-    } catch (e) {
-      console.log(e);
-      next(e);
-    }
-  },
-
+const assetManagementController = {
   addDevice: async (req, res, next) => {
     try {
-      const device = await Module.create({ ...req.body });
+      const device = await Device.create({ ...req.body });
 
       if (!device) throw new ApiError("Error creating asset", 400);
 
@@ -73,69 +22,185 @@ const DeviceController = {
     try {
       const { deviceId } = req.params;
 
-      const asset = await Asset.findOne({ where: { id: deviceId } });
+      const device = await Device.findOne({ where: { id: deviceId } });
 
-      if (!asset) throw new ApiError("Device not found", 404);
+      if (!device) throw new ApiError("Device not found", 404);
+
+      await device.update({ ...req.body });
 
       return res.status(200).json(ApiResponse("DEVICE updated successfully"));
     } catch (e) {
       console.log(e);
     }
   },
+  deleteDevice: async (req, res, next) => {
+    try {
+      const { deviceId } = req.params;
+      await Device.destroy({ where: { id: deviceId } });
+
+      return res.status(200).json(ApiResponse("Device deleted successfully"));
+    } catch (e) {
+      console.log(e);
+      next(e);
+    }
+  },
 
   getAllDevice: async (req, res, next) => {
     try {
-      const device = await Device.findAll();
+      const devices = await Device.findAll({
+        include: [
+          {
+            model: User,
+            include: [
+              { model: Role, as: "role" },
+              { model: Department, as: "department" },
+            ],
+          },
+        ],
+      });
+
+      // console.log(devices);
 
       return res
         .status(200)
-        .json(ApiResponse("Device fetched successfully", "device", Device));
+        .json(ApiResponse("Devices fetched successfully", "devices", devices));
     } catch (e) {
       console.log(e);
       next(e);
     }
   },
-  editUser: async (req, res, next) => {
+
+  addLicenseSubscription: async (req, res, next) => {
     try {
-      const { userId } = req.params;
-
-      const user = await User.findOne({ where: { id: userId } });
-
-      if (!user) throw new ApiError("Error updating a user", 404);
-
-      await user.update({...req.body})
-
-      return res.status(200).json(ApiResponse("User updated successfully"));
-    } catch (e) {
-      console.log(e);
-    }
-  },
-  deleteUser: async (req, res, next) => {
-    try {
-      const { userId } = req.params;
-
-      await User.destroy({ where: { id: userId } });
-
-      return res.status(200).json({
-        success: false,
-        message: "User deleted successfully"
+      const licensesubscription = await LicenseSubscription.create({
+        ...req.body,
       });
+
+      if (!licensesubscription) throw ApiError("Error creating asset", 400);
+
+      return res.status(201).json(ApiResponse("Asset created successfully"));
     } catch (e) {
       console.log(e);
       next(e);
     }
   },
-  getAllUsers: async (req, res, next) => {
+  editLicenseSubscription: async (req, res, next) => {
     try {
-      const users = await User.findAll();
+      const { licensesubscriptionId } = req.params;
 
-      return res.status(200).json(ApiResponse("Users fetched", "users", users));
+      const licensesubscription = await LicenseSubscription.findOne({
+        where: { id: licensesubscriptionId },
+      });
+      if (!licensesubscription) throw new ApiError("License not found", 404);
+
+      return res
+        .status(200)
+        .json(ApiResponse("licensesubscription updated successfully"));
+    } catch (e) {
+      console.log(e);
+    }
+  },
+  deleteLicenseSubscription: async (req, res, next) => {
+    try {
+      const { licensesubscriptionId } = req.params;
+      await LicenseSubscription.destroy({
+        where: { id: licensesubscriptionId },
+      });
+      return res
+        .status(200)
+        .json(ApiResponse("LicenseSubscription updated successfully"));
     } catch (e) {
       console.log(e);
       next(e);
     }
-  }
-  
+  },
+  getAllLicenseSubscription: async (req, res, next) => {
+    try {
+      const licensesubscriptions = await LicenseSubscription.findAll({
+        include: [{
+          model: User,
+          include: [
+            { model: Role, as: "role" },
+            { model: Department, as: "department" },
+          ],
+        },
+      ],
+    });
+      console.log(licensesubscriptions);
+      return res
+        .status(200)
+        .json(
+          ApiResponse(
+            "LicenseSubscription fetched successfully",
+            "licensesubscriptions",
+            licensesubscriptions
+          )
+        );
+    } catch (e) {
+      console.log(e);
+      next(e);
+    }
+  },
+  addSimcards: async (req, res, next) => {
+    try {
+      const simcards = await Simcards.create({ ...req.body });
+      if (!simcards) throw ApiError("Error creating asset", 400);
+
+      return res.status(201).json(ApiResponse("Asset created successfully"));
+    } catch (e) {
+      console.log(e);
+      next(e);
+    }
+  },
+  editSimcards: async (req, res, next) => {
+    try {
+      const { simcardsId } = req.params;
+
+      const simcards = await Simcards.findOne({
+        where: { id: simcardsId },
+      });
+      if (!simcards) throw new ApiError("simcards not found", 404);
+
+      return res.status(200).json(ApiResponse("simcards updated successfully"));
+    } catch (e) {
+      console.log(e);
+    }
+  },
+  deleteSimcards: async (req, res, next) => {
+    try {
+      const { simcardsId } = req.params;
+      await Simcards.destroy({
+        where: { id: simcardsId },
+      });
+      return res.status(200).json(ApiResponse("Simcards updated successfully"));
+    } catch (e) {
+      console.log(e);
+      next(e);
+    }
+  },
+  getAllSimcards: async (req, res, next) => {
+    try {
+      const simcards = await Simcards.findAll({ include: [{
+        model: User,
+        include: [
+          { model: Role, as: "role" },
+          { model: Department, as: "department" },
+        ],
+      },
+    ],
+  });
+
+      console.log(simcards);
+      return res
+        .status(200)
+        .json(
+          ApiResponse("Simcards fetched successfully", "simcards", simcards)
+        );
+    } catch (e) {
+      console.log(e);
+      next(e);
+    }
+  },
 };
 
-asset.exports = DeviceController;
+module.exports = assetManagementController;
