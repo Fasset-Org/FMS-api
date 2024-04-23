042b5540ddb657e2a32c9027c8af8e4fe6f8628d
SiyamthandaNyathi <131876850+SiyamthandaNyathi@users.noreply.github.com>
2024-04-23 10:59:07 +0200

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -3,12 +3,13 @@ const authRouter = require("./auth/auth");
 const adminRouter = require("./admin/admin");
 const scmRouter = require("./user/scm");
 const HumanResourceRouter = require("./user/humanResource");
+const assetManagementRouter = require("./user/assetManagement");
 
 const appRouters = Router();
 
 appRouters.use("/auth", authRouter);
 appRouters.use("/admin", adminRouter);
 appRouters.use("/scm", scmRouter);
-appRouters.use('/humanResource', HumanResourceRouter);
-
+appRouters.use("/humanResource", HumanResourceRouter);
+appRouters.use("/assetManagement", assetManagementRouter);
 module.exports = appRouters;
