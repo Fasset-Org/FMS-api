1d2b2f6a723d1313919acff1ade369f0df195c72
Kamzen <37660966+Kamzen@users.noreply.github.com>
2025-01-29 16:55:15 +0200

    Update emailTemplates.js

--- utils/emailTemplates.js
+++ utils/emailTemplates.js
@@ -11,9 +11,22 @@ const EmailTemplates = {
                 <p>Kind Regards,</p><p>Human Resources Department Team</p><p>1st Floor, 296 Kent Avenue, Ferndale, Randburg, 2194 
                 PO Box 6801, Cresta, 2118</p>
                 <br />
-                <img src="${process.env.API_URL}/images/footer-image.jpg" alt="" />
+                <img src="${
+                  process.env.API_URL
+                }/images/footer-image.jpg" alt="" />
                 <br />
                 `;
+  },
+  autoRejectEmail: (gender, lastName, position) => {
+    return `<p>Dear ${gender === "Male" ? "Mr" : "Ms"} ${" "} ${lastName}</p>
+            <p>Thank you for applying for ${position}. There have been several applications and after careful consideration we decided to continue with other 
+         applications. We thank you sincerely for your efforts and wish you all the best in your job search and securing a new role.</p>
+         <p>We do appreciate your interest in us, and we would like to retain your details on file should a more suitable 
+          position arise.</p>
+            <br />
+            <img src="${process.env.API_URL}/images/footer-image.jpg" alt="" />
+            <br />
+          `;
   }
 };
 
